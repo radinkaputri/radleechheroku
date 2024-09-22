88a64574b2a206407ff8b742b53c01444ce9fab0
clyfly <deni@xyradcloud.my.id>
2024-09-22 11:06:56 +0700

    db connection
    Signed-off-by: clyfly <deni@xyradcloud.my.id>

--- update.py
+++ update.py
@@ -50,7 +50,7 @@ if len(DATABASE_URL) == 0:
 
 if DATABASE_URL:
     conn = MongoClient(DATABASE_URL)
-    db = conn.z
+    db = conn.mltb
     if config_dict := db.settings.config.find_one({'_id': bot_id}):
         environ['UPSTREAM_REPO'] = config_dict['UPSTREAM_REPO']
         environ['UPSTREAM_BRANCH'] = config_dict['UPSTREAM_BRANCH']
